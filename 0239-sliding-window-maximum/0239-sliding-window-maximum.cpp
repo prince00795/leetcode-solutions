35ee1897e12d84f23ac96aff6b5f9809038823cc
prince00795 <princeyadav843123@gmail.com>
2026-01-19 17:03:26 +0530

    Time: 27 ms (44.29%), Space: 139.1 MB (87.8%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-
-        //next grater index
-         int n=arr.size();
-        vector<int>ngi(n);
-        stack<int >st;
-        ngi[n-1]=n;
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(st.size()>0 && arr[st.top()]<=arr[i]) st.pop();
-            if(st.size()==0) ngi[i]=n;
-          else  ngi[i]=st.top();
-            st.push(i);
-        }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
+        deque<int>dq;
+        int n=nums.size();
         vector<int>ans;
-        int j=0;
-        for(int i=0;i<=n-k;i++){
-             if(j < i) j = i;
-           int mx= arr[j];
-           while(j<i+k){ //if nge is inside the window
-            mx=arr[j];
-            if(ngi[j]>=i+k) break;
-            j=ngi[j];
-           }
-           ans.push_back(mx);
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && nums[i]>nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            int j=i-k+1 ;// start of window
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1)  ans.push_back(nums[dq.front()]);
         }
         return ans;
     }
