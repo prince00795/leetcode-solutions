f47d78976ea21154def59ac9018aa7de36280281
prince00795 <princeyadav843123@gmail.com>
2026-02-03 01:42:23 +0530

    Time: 0 ms (100%), Space: 11.2 MB (95.32%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -1,41 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-void inorder(TreeNode* root,vector<int>&ans){
-    if(root==NULL) return;
-    inorder(root->left,ans);
-    ans.push_back(root->val);
-    inorder(root->right,ans);
+    int sum=0;
+    void reverseInorder(TreeNode* root){
+        if(root==NULL) return ;
+        reverseInorder(root->right);
+        sum+=root->val;
+        root->val=sum;
+        reverseInorder(root->left);
 
-}
- 
- TreeNode* helper(TreeNode* root,vector<int>ans){
-     if(root==NULL) return root;
-          int sum=0;
-          int idx=0;
-          for(int i=0;i<ans.size();i++){
-            if(root->val==ans[i]) {
-                idx=i;
-                break;
-            }
-
-          }
-          for(int i=idx;i<ans.size();i++){
-            sum+=ans[i];
-          }
-          root->val=sum;
-
-         helper(root->left,ans);
-         helper(root->right,ans);
-         return root;
-
- }
-
+    }
     TreeNode* bstToGst(TreeNode* root) {
-        if(root==NULL) return root;
-          vector<int>ans;
-          inorder(root,ans);
-          
-          return helper(root,ans);
+        if(root==NULL) return NULL;
 
+        reverseInorder(root);
+        return root;
     }
 };
