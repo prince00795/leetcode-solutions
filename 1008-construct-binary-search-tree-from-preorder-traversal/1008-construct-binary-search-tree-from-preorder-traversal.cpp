a36c0ab644382e2333c82348e3e0ea13870d9af9
prince00795 <princeyadav843123@gmail.com>
2026-02-03 16:42:48 +0530

    Time: 0 ms (100%), Space: 16.6 MB (21.41%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,28 @@
+ 
+class Solution {
+public:
+    TreeNode* bulid(vector<int>& pre, int prelo,int prehi,vector<int>& in,int inlo,int inhi){
+        if(prelo>prehi) return NULL;
+        TreeNode* root=new TreeNode(pre[prelo]);
+        if(prelo==prehi) return root;
+        int i=inlo;
+        while(i<=inhi){
+            if(in[i]==pre[prelo]) break;
+            i++;
+        }
+        int leftcount=i-inlo;
+        int rightcount=inhi-i;
+        root->left=bulid(pre,prelo+1,prelo+leftcount,in,inlo,i-1);
+        root->right=bulid(pre,prelo+leftcount+1,prehi,in,i+1,inhi);
+        return root;
+    }
+
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+        vector<int>in=pre;
+        sort(in.begin(),in.end());
+         int n=pre.size();
+        return bulid(pre,0,n-1,in,0,n-1);
+
+        
+    }
+};
