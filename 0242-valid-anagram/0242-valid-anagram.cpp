4b05d60b82c4f2003a330a733d9f2533a9932d85
prince00795 <princeyadav843123@gmail.com>
2026-02-20 18:39:59 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,23 +3,19 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
         unordered_map<char,int>map1;
-        unordered_map<char,int>map2;
 
         for(int i=0;i<s.length();i++){
             map1[s[i]]++;
         }
-        for(int i=0;i<t.length();i++){
-            map2[t[i]]++;
-        }
-        for(auto x : map1){
-            char ch1=x.first;
-            int freq1=x.second;
-            if(map2.find(ch1)!=map2.end()){
-                int freq2=map2[ch1];
-                if(freq2!=freq1) return false;
+         
+         for(int i=0;i<t.length();i++){
+            if(map1.find(t[i])!=map1.end()){
+                int freq=map1[t[i]];
+                if(freq==1) map1.erase(t[i]);
+                else map1[t[i]]--;
             }
             else return false;
-        }
+         }
         return true;
     }
 };
