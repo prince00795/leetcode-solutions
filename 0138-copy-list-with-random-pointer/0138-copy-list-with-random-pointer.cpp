867dcf3ab13af565ef8f19151c0449504fd723f9
prince00795 <princeyadav843123@gmail.com>
2026-03-02 16:48:11 +0530

    Time: 11 ms (15.39%), Space: 15 MB (69.74%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,37 +1,39 @@
+ 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head) return NULL;
-
-        // ===== STEP 1: Create copy nodes and interleave =====
-        Node* curr = head;
-        while (curr) {
-            Node* copy = new Node(curr->val);
-            copy->next = curr->next;
-            curr->next = copy;
-            curr = copy->next;
+        
+        //deep copy without random pointer
+        Node* dummy =new Node(-1);
+        Node* tempc=dummy;
+        Node* temp=head;
+        while(temp!=NULL){
+            tempc->next=new Node(temp->val);
+            temp=temp->next;
+            tempc=tempc->next;
         }
+        Node* b=dummy->next;
+        Node* a=head;
+        //maake  map of original node and dublicate node
+        unordered_map<Node*,Node*>m;
 
-        // ===== STEP 2: Assign random pointers =====
-        curr = head;
-        while (curr) {
-            if (curr->random)
-                curr->next->random = curr->random->next;
-            curr = curr->next->next;
-        }
+         Node* tempa=a;
+         Node* tempb=b;
+         while(tempa!=NULL){
+            m[tempa]=tempb;
+            tempa=tempa->next;
+            tempb=tempb->next;
+         }
+         //ab random ponter marking 
+         for(auto x: m){
+            Node* o=x.first;
+            Node* d=x.second;
+            if(o->random!=NULL){
+                 d->random=m[o->random];
+            }
+ 
+         }
+         return b;
 
-        // ===== STEP 3: Separate original and copied list =====
-        Node* dummy = new Node(0);
-        Node* copyCurr = dummy;
-        curr = head;
-
-        while (curr) {
-            copyCurr->next = curr->next;
-            curr->next = curr->next->next;
-            copyCurr = copyCurr->next;
-            curr = curr->next;
-        }
-
-        return dummy->next;
     }
 };
