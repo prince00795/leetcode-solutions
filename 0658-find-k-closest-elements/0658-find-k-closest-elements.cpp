0d73f1367b412b2327d002c8a3319626e0eb6524
prince00795 <princeyadav843123@gmail.com>
2026-03-09 23:24:08 +0530

    Time: 31 ms (15.52%), Space: 38.8 MB (20.53%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,76 +1,19 @@
 class Solution {
 public:
+typedef pair<int,int>pi;
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n=arr.size();
-        vector<int>v(k);
-        if(arr[0]>x){
-            for(int i=0;i<k;i++){
-                v[i]=arr[i];
-            }
-            return v;
+        priority_queue<pi>pq;
+        for(int i=0;i<arr.size();i++){
+            int distance=abs(x-arr[i]);
+            pq.push({distance,arr[i]});
+            if(pq.size()>k)pq.pop();
         }
-        if(arr[n-1]<x){
-            while(k>0){
-                v[k-1]=arr[n-1];
-                k--;
-                n--;
-            }
-            return v;
+        vector<int>ans;
+        while(pq.size()>0){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-        int lo=0;
-        int hi=n-1;
-        bool flag=false;
-        int t=0;
-        int mid=-1;
-         
-        while(lo<=hi){
-          mid=lo+(hi-lo)/2;
-            if(arr[mid]==x){
-                flag=true;
-                v[t]=arr[mid];
-                t++;
-                break;
-            }
-            else if(arr[mid]>x) hi=mid-1;
-             else if(arr[mid]<x) lo=mid+1;
-        }
-        int lb=hi;
-        int ub=lo;
-        if(flag==true){
-             lb=mid-1;
-               ub=mid+1;
-        }
-        while(t<k && lb>=0 && ub<=n-1){
-            int d1=abs(x-arr[lb]);
-            int d2=abs(x-arr[ub]);
-            if(d1<=d2){
-                v[t]=arr[lb];
-                t++;
-                lb--;
-            }
-            else{
-                v[t]=arr[ub];
-                t++;
-                ub++;
-
-            }
-        }
-        if(lb<0){
-            while(t<k){
-                v[t]=arr[ub];
-                t++;
-                ub++;
-            }
-        }
-        if(ub>n-1){
-            while(t<k){
-                v[t]=arr[lb];
-                t++;
-                lb--; 
-            }
-        }
-        sort(v.begin(),v.end());
-        return v;
-
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
