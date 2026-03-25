4466d610294867777036db57df20a5252dea0a55
prince00795 <princeyadav843123@gmail.com>
2026-03-25 23:59:45 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.49%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
+    vector<int>dp;
     int helper(int n){
+        if( dp[n]!=-1) return dp[n];
         if(n==0) return 1;
         if(n==1) return 1;
         if(n==2) return 2;
@@ -8,9 +10,10 @@ public:
         for(int i=1;i<=n;i++){
             total+=helper(i-1)*helper(n-i);
         }
-        return total;
+        return dp[n]= total;
     }
     int numTrees(int n) {
+        dp.resize(30,-1);
         return helper(n);
 
     }
