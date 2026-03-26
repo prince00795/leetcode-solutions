ed7d25de4ffdf5e61dd72376401b1b469aad96ad
prince00795 <princeyadav843123@gmail.com>
2026-03-26 23:18:29 +0530

    Time: 21 ms (14.66%), Space: 15.8 MB (65.55%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     int dp[1005][105][2];
+    int helper(int k, vector<int>& arr,int i,bool canbuy){
+        if(i == arr.size()) return 0;
+        if(k==0) return 0;
+        if(dp[i][k][canbuy]!=-1) return dp[i][k][canbuy];
+        int profit=0;
+        if(canbuy==true) profit= max({-arr[i]+helper(k,arr,i+1,false),helper(k,arr,i+1,true)});
+        if(canbuy==false) profit= max({arr[i]+helper(k-1,arr,i+1,true),helper(k,arr,i+1,false)});
+
+        return  dp[i][k][canbuy]=profit;
+    }
+    int maxProfit(int k, vector<int>& arr) {
+        memset(dp,-1,sizeof dp);
+        return helper(k,arr,0,true);
+    }
+};
