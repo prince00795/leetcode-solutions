d83ab87d58e57e7455af28cb1857b24b0262a091
prince00795 <princeyadav843123@gmail.com>
2026-08-03 19:11:40 +0530

    Time: 15 ms (5.78%), Space: 17 MB (73.81%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,43 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int level(TreeNode* root){
         if(root==NULL) return 0;
         return 1+max(level(root->left),level(root->right));
     }
-    void sol(TreeNode* root,int currlev,int reqlev,vector<int>&ans){
-        if(root==NULL) return ;
-        if(currlev==reqlev){
+    void nthlevel(TreeNode* root,int currlevel ,int reqlevel,vector<int>&ans){
+        if(root==NULL) return;
+        if(currlevel==reqlevel){
             ans.push_back(root->val);
-            return;
         }
-
-        sol(root->left,currlev+1,reqlev,ans);
-        sol(root->right,currlev+1,reqlev,ans);
+        nthlevel(root->left,currlevel+1,reqlevel,ans);
+        nthlevel(root->right,currlevel+1,reqlevel,ans);
 
     }
-     
     vector<vector<int>> levelOrder(TreeNode* root) {
-         int n=level(root);
-         vector<vector<int>>fans;
-        vector<int>ans;
-         for(int i=1;i<=n;i++){
-            sol(root,1,i,ans);
-            fans.push_back(ans);
-            ans.clear();
+        int n=level(root);
+        vector<vector<int>>fians;
+        for(int i=1;i<=n;i++){
+            vector<int>ans;
+            nthlevel(root,1,i,ans);
+            fians.push_back(ans);
         }
-
-        return fans;
-        
+        return fians;
     }
 };
